a7fe3693e292529e69257715576173c10c2aaef3
aniketchakravarty <rock.aniket00@gmail.com>
2023-12-17 23:10:46 +0530

    schemas added

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,73 @@
+// Import the Mongoose library
+const mongoose = require("mongoose");
 
+// Define the user schema using the Mongoose Schema constructor
+const userSchema = new mongoose.Schema(
+	{
+		// Define the name field with type String, required, and trimmed
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		// Define the email field with type String, required, and trimmed
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+		},
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+		// Define the password field with type String and required
+		password: {
+			type: String,
+			required: true,
+		},
+		// Define the role field with type String and enum values of "Admin", "Student", or "Visitor"
+		accountType: {
+			type: String,
+			enum: ["Customer", "Seller"],
+			required: true,
+		},
+		active: {
+			type: Boolean,
+			default: true,
+		},
+		approved: {
+			type: Boolean,
+			default: true,
+		},
+		additionalDetails: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "Profile",
+		},
+		books: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Books",
+			},
+		],
+		token: {
+			type: String,
+		},
+		resetPasswordExpires: {
+			type: Date,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+	
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+		// Add timestamps for when the document is created and last modified
+	},
+	{ timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+// Export the Mongoose model for the user schema, using the name "user"
+module.exports = mongoose.model("user", userSchema);
