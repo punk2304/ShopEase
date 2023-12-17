e3fcc3dbc1f48596fb01b45f56e37f76377c4d86
Himanshu <hchhabra419@gmail.com>
2023-12-17 22:05:57 +0530

    login

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -23,4 +23,47 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.get('/login', async(req, res) =>{
+  try{
+        const {email, password} = req.body;
+
+        // Check if email or password is missing
+        if (!email || !password) {
+        
+          // Return 400 Bad Request status code with error message
+          return res.status(400).json({
+            success: false,
+            message: `Please Fill up All the Required Fields`,
+          });
+        }
+
+        
+        // Find user with provided email
+        const user = await User.findOne({ email });
+
+        // If user not found with provided email
+        if (!user) {
+          // Return 401 Unauthorized status code with error message
+          return res.status(401).json({
+            success: false,
+            message: `User is not Registered with Us Please SignUp to Continue`,
+          });
+        }
+        else{
+            return res.status(200).json({
+              success: true,
+              message: `User is logged in successfully`
+            });
+        }
+  }
+  catch (error) {
+      console.error(error);
+      // Return 500 Internal Server Error status code with error message
+      return res.status(500).json({
+        success: false,
+        message: `Login Failure Please Try Again`,
+      });
+	}
+});
+
 module.exports = router;
