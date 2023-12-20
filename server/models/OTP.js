eb36c5c60200215b0e18a9abd1666ea1f76590e0
Himanshu <hchhabra419@gmail.com>
2023-12-21 02:23:04 +0530

    templates added

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
+const otpSignUp = require("../templates/otpSignUp")
+
 // const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 const OTPSchema = new mongoose.Schema({
 	// Define the email field with type String, required, and trimmed
@@ -31,7 +33,7 @@ async function sendVerificationEmail(email, otp) {
 		const mailResponse = await mailSender(
 			email,
 			"Verification Email",
-			otp
+			otpSignUp(otp)
 		);
 		console.log("Email sent successfully: ", mailResponse.response);
 	} catch (error) {
@@ -43,12 +45,43 @@ async function sendVerificationEmail(email, otp) {
 // Define a post-save hook to send email after the document has been saved
 OTPSchema.pre("save", async function (next) {
 	console.log("New document saved to database");
-
 	
 		await sendVerificationEmail(this.email, this.otp);
 
 	next();
 });
+
+
+
+// // Define a function to send emails for changing password
+// async function sendChangePasswordEmail(email, otp) {
+// 	// Create a transporter to send emails
+
+// 	// Define the email options
+
+// 	// Send the email
+// 	try {
+// 		const mailResponse = await mailSender(
+// 			email,
+// 			"Verification Email",
+// 			otpSignUp(otp)
+// 		);
+// 		console.log("Email sent successfully: ", mailResponse.response);
+// 	} catch (error) {
+// 		console.log("Error occurred while sending email: ", error);
+// 		throw error;
+// 	}
+// }
+
+// // Define a post-save hook to send email after the document has been saved
+// OTPSchema.pre("save", async function (next) {
+// 	console.log("New document saved to database");
+	
+// 		await sendChangePasswordEmail(this.email, this.otp);
+
+// 	next();
+// });
+
 
 const OTP = mongoose.model("OTP", OTPSchema);
 
